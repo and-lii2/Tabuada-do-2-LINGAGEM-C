0518798dfe34cff94ca05e4d7b0ddee5ff1dc445
Anderson Lima Moura <79551292+and-lii2@users.noreply.github.com>
2021-03-02 22:44:39 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int num = 2;
+	int count = 1;
+	
+	while(count <= 10)
+      {
+            printf("%i * %i = %i\n", num, count, num*count);
+            count++;
+      } 
+}
